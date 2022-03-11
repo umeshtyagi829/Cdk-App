dfe1e04daf3dc388a8ef7988b0230bc7e0774660
Umesh <utyagi@presidio.com>
2022-03-11 18:22:25 +0530

    updated

--- bin/deploy_ecs.ts
+++ bin/deploy_ecs.ts
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { DeployEcsStack } from '../lib/deploy_ecs-stack';
-import { DynamoDbStack } from '../lib/dynamodb';
+// import { DeployEcsStack } from '../lib/deploy_ecs-stack';
+// import { DynamoDbStack } from '../lib/dynamodb';
+import  { PipelineStack } from '../lib/code_pipeline';
 
 const app = new cdk.App();
-new DeployEcsStack(app, 'DeployEcsStack', {
-  // env: { account: '123456789012', region: 'us-east-1' },
-});
+// new DeployEcsStack(app, 'DeployEcsStack', {
+//   // env: { account: '123456789012', region: 'us-east-1' },
+// });
 
-new DynamoDbStack(app, 'DynamoDbStack', {
-  // env: { account: '123456789012', region: 'us-east-1' },
-});
+// new DynamoDbStack(app, 'DynamoDbStack', {
+//   // env: { account: '123456789012', region: 'us-east-1' },
+// });
+
+new PipelineStack (app, 'CdkPipelineStack');
---
+++ lib/code_pipeline.ts
@@ -0,0 +1,27 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, CodeBuildStep } from 'aws-cdk-lib/pipelines';
+
+export class PipelineStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            // The pipeline name
+            pipelineName: 'CdkPipeline',
+
+            // How it will be built and synthesized
+            synth: new CodeBuildStep('Synth', {
+                // Where the source can be found
+                input: CodePipelineSource.gitHub('umeshtyagi829/cdk-pipeline-demo', 'master'),
+
+                // Install dependencies, build and run cdk synth
+                commands: [
+                    'npm ci',
+                    'npm run build',
+                    'npx cdk synth'
+                ],
+            }),
+        });
+    }
+}
